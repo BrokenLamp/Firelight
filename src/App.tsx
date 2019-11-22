88e8670f040e46bda2f1e9bed5afcaac1ab6b585
Brandon Dyer <BrandonDyer64@gmail.com>
2019-11-21 20:23:24 -0600

    Added game screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import match from "./patternMatch";
+import GameScreen from "./components/screens/GameScreen";
 import TitleScreen from "./components/screens/TitleScreen";
 import "./App.css";
 
 const core: any = window.require("core");
 
 export default () => {
-    const [screen, setScreen] = useState("title");
-    const screenComponent = match<string, JSX.Element>(screen)([
-        "title",
-        () => <TitleScreen setScreen={setScreen} />,
-    ]);
+    const [screen, setScreen] = useState(process.env.FL_SCREEN || "title");
+    const screenComponent = match<string, JSX.Element>(screen)(
+        ["game", () => <GameScreen setScreen={setScreen} />],
+        ["title", () => <TitleScreen setScreen={setScreen} />],
+    );
     return <div className="App">{screenComponent}</div>;
 };
---
+++ src/components/screens/GameScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./GameScreen.module.css";
+
+export default (props: any) => {
+    return (
+        <div className={styles.GameScreen}>This is the main game screen!</div>
+    );
+};
