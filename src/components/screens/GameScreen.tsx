ac15fcddeefe150b5aa236cab1f97f9be3231669
Brandon Dyer <brandondyer64@gmail.com>
2019-12-09 17:52:33 -0600

    Added Game Screen layouts

--- src/components/screens/GameScreen.tsx
+++ src/components/screens/GameScreen.tsx
@@ -3,6 +3,19 @@ import styles from "./GameScreen.module.css";
 
 export default (props: any) => {
     return (
-        <div className={styles.GameScreen}>This is the main game screen!</div>
+        <div className={styles.GameScreen}>
+            <div className={styles.Head}>
+                HEAD
+            </div>
+            <div className={styles.Log}>
+                LOG
+            </div>
+            <div className={styles.Main}>
+                MAIN
+            </div>
+            <div className={styles.Map}>
+                MAP
+            </div>
+        </div>
     );
 };
